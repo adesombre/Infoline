9e3ec0b801d7284df870f7a97a6b98e77643ddaf
desombre <alaindesombre@gmail.com>
2022-08-25 11:42:14 +0200

    add swagger of nest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {ApiBearerAuth,
+        ApiOperation,
+        ApiParam,
+        ApiResponse,
+       ApiTags} from'@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({summary: 'return hello-world'})
+  @ApiResponse({status:400 ,description:'forbiden'})
+  @ApiResponse({status:200 ,description:'create'})
+  @ApiTags('infoline')
   getHello(): string {
     return this.appService.getHello();
   }
